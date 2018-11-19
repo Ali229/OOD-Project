af3fd514779e056e0922dbaffd3ffb7c7c71bd05
Muhammad Ali <alinaeem229@gmail.com>
2018-11-19 15:59:56 -0500

    Front-end reinit

---
+++ Front-end/premium-travel/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+	MDBBootstrapModule.forRoot(),
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
