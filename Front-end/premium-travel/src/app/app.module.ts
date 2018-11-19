9b750bdf3ab8181496a6245c9bf24448edc24941
Muhammad Ali <alinaeem229@gmail.com>
2018-11-19 16:28:59 -0500

    Added navbar and AngularMDB

--- Front-end/premium-travel/src/app/app.module.ts
+++ Front-end/premium-travel/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-	MDBBootstrapModule.forRoot(),
+    MDBBootstrapModule.forRoot(),
     BrowserModule
   ],
   providers: [],
