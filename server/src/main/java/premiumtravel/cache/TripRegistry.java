9101e12ea14b286c232a43609ff0f8aa39fcc698
Mark Zeagler <mark.zeagler@outlook.com>
2018-11-26 09:23:42 -0500

    Made TripRegistry final

--- server/src/main/java/premiumtravel/cache/TripRegistry.java
+++ server/src/main/java/premiumtravel/cache/TripRegistry.java
@@ -2,7 +2,7 @@ package premiumtravel.cache;
 
 import premiumtravel.trip.Trip;
 
-public class TripRegistry extends Registry<Trip> {
+public final class TripRegistry extends Registry<Trip> {
 	private static final long serialVersionUID = 3095507398182883303L;
 
 	private TripRegistry() {
