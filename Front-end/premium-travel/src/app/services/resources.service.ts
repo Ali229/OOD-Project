5c43cd02aa3a31a46e36988097e00c79bb1f82df
Muhammad Ali <alinaeem229@gmail.com>
2018-11-26 17:57:13 -0500

    Fixes lints

--- Front-end/premium-travel/src/app/services/resources.service.ts
+++ Front-end/premium-travel/src/app/services/resources.service.ts
@@ -1,30 +1,30 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResourcesService {
 
-  private readonly backendAddress: string = "https://premium-travel.markzeagler.com/resources/";
+  private readonly backendAddress: string = 'https://premium-travel.markzeagler.com/resources/';
 
   constructor(private http: HttpClient) {
 
   }
 
   public getAllAgentsObservable() {
-    return this.http.get(this.backendAddress + "travel-agent")
+    return this.http.get(this.backendAddress + 'travel-agent');
   }
 
   public getAgent(id: string) {
-    return this.http.get(this.backendAddress + "travel-agent/" + id)
+    return this.http.get(this.backendAddress + 'travel-agent/' + id);
   }
 
   public createAgent(firstName: string, lastName: string, phoneNumber: string) {
-    return this.http.post(this.backendAddress + "travel-agent", {
-      "firstName": firstName,
-      "lastName": lastName,
-      "phoneNumber": phoneNumber
-    })
+    return this.http.post(this.backendAddress + 'travel-agent', {
+      'firstName': firstName,
+      'lastName': lastName,
+      'phoneNumber': phoneNumber
+    });
   }
 }
